4c0d7b3efde14a4c876cf7131e809ae9c2e25ac5
AMINUL ISLAM <lostvayne684@gmail.com>
2025-05-23 18:34:11 +0530

    added headers to avoid crashing in linux

--- scrap_it.py
+++ scrap_it.py
@@ -9,8 +9,12 @@ app = Flask(__name__)
 
 def extract_coordinates(map_url):
     options = Options()
-    options.add_argument("--headless=new") 
+    options.add_argument("--headless=new")
     options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-setuid-sandbox")
+    options.add_argument("--remote-debugging-port=9222") 
     options.add_argument("--window-size=1920,1080")
 
     driver = webdriver.Chrome(options=options)
